e4a79534e94879c1ac48fc461f9001465ff1e7cb
manzim <manzim.ruet@gmail.com>
2021-04-10 00:40:55 +0600

    project initial commit

---
+++ game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Game
+      </header>
+    </div>
+  );
+}
+
+export default App;
